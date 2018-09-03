2062c6278d47562c2682d3579581bc8e79a30fab
gyn7561 <200804632@qq.com>
2018-09-03 10:54:39 +0800

    support multi coin ctl , support 5 ticker source

--- test/test.js
+++ test/test.js
@@ -1,41 +1,31 @@
-let config = {
-    curreny: "USD",
-    coin: "ethereum",
-    //initPrice: 500,
-    collateralCoinCount: (0.1830235864318801 + 0.17058353380320057 + 0.9980547392231892),
-    debt: 366.0851651171458
-};
-var queryCTL = require("../index");
 
-//queryCTL(config).then(r => {
-//    console.log(r);
-//});
+const queryCTL = require("../index");
+
+let config = {
+    assets: [
+        {
+            coin: "ethereum",
+            sym: "eth",
+            count: 0.5
+        },
+        {
+            coin: "bitcoin",
+            sym: "btc",
+            count: 0.1
+        }
+    ],
+    debt: 400
+};
+
 
 let margincall = 2;
 let margincall_callback = (r) => {
     console.log("alert!! " + r);
 }
 
-//queryCTL(config, margincall, margincall_callback).then(r => {
-//    console.log(r);
-//});
-
-let config2 = {
-    curreny: "USD",
-    coin: "ethereum",
-    collateralCoinCount: (0.1830235864318801 + 0.17058353380320057 + 0.9980547392231892),
-    debt: 366.0851651171458,
-    cache: true,
-    expire: 1000
-};
-
 async function test() {
-    while (true) {
-        var rate = await queryCTL(config2, margincall, margincall_callback);
-        console.log(rate);
-    }
-    
+    var rate = await queryCTL(config, margincall, margincall_callback);
+    console.log(rate);
 }
 
 test();
- 
